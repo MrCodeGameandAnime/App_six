cb73538da2e18c02ac10ff55c34c0036e5d51036
MrCodeGameandAnime <mrcodegameandanime@gmail.com>
2021-01-06 07:49:07 -0500

    first commit

---
+++ app_six/resources/views/layouts/menu.blade.php
@@ -0,0 +1,10 @@
+<!-- need to remove -->
+<li class="nav-item">
+    <a href="{{ route('home') }}" class="nav-link active">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Home</p>
+    </a>
+    <a href="{{ route('home') }}" class="nav-link">
+        <p>Todo</p>
+    </a>
+</li>
